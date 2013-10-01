8699df608d8e7bed7eefc51844ac307c1c9c0bdb
Calvin Winkowski <telnoratti@gmail.com>
2013-10-01 14:53:18 -0400

    Add debugging print macro.
    This adds a macro DEBUG_PRINT which can be used to easily enable/disable
    debugging printing. See file for info on how to use.

---
+++ esh/src/debug.h
@@ -0,0 +1,12 @@
+/* Remove to turn of debugging */
+#define DEBUG
+
+#ifdef DEBUG
+# define DEBUG_PRINT(x) printf x
+#else
+# define DEBUG_PRINT(x) do {} while (0)
+#endif
+/*
+ * This can be used like:
+ *     DEBUG_PRINT(("var1: %d; var2: %d; str: %s\n", var1, var2, str));
+ */
